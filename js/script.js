38eaf00462e800bd875446adc27e6cf22d581d7d
Wendy Jiang <wjiang.student@hsfi.us>
2017-12-11 23:15:35 +0000

    fix java

--- js/script.js
+++ js/script.js
@@ -1,25 +1,89 @@
 //Class 1:
-        // Fork and clone the repo and set up your project workspace and link files
-        // Write HTML for your project
+// Fork and clone the repo and set up your project workspace and link files
+// Write HTML for your project
 //Class 2:  
-        // Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
-        // Practice using console.log() and debugger to debug your code
-        // Create your document ready function. 
-        // Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
-        // Display the user choice to the output screen
+// Declare four global variables for intial state of the list of choices, the computer choice, the user choice, and the winner.  
+// Practice using console.log() and debugger to debug your code
+// Create your document ready function. 
+// Create a click hander for when the "Shoot!" button is clicked, takes in the user choice from the input field, 
+// Display the user choice to the output screen
 //Class 3:  
-        // Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
-        // Display the computer choice to the output screen
-        // Compare the user choice and the computer choice to determine who won. 
-        // Display the user winner to the output screen 
-        // Clear the input box for the next choice
+// Randomly choose among 'rock', 'paper', or 'scissors' from the list of choices 
+// Display the computer choice to the output screen
+// Compare the user choice and the computer choice to determine who won. 
+// Display the user winner to the output screen 
+// Clear the input box for the next choice
 
 // *************************** YOUR CODE BELOW *******************************
-$(document).ready(function(){
-        var userChoice='';
-        var computerChoice='';
-        var winner='';
-        var choices=["rock","paper", "scissors"]
+$(document).ready(function() {
+
+$("#userChoice").click(function() {
+
+                var userChoice = $("input").val().toLowerCase();
+
+                var choices = ["rock", "paper", "scissors"];
+
+                if (userChoice === choices[0]) {
+
+                        $("#uc").html('<img src="images/rock.png">');
+                        $("#uc").append("rock");
+
+                }
+                else if (userChoice === choices[1]) {
+
+                        $("#uc").html('<img src="images/paper.png">');
+
+                }
+                else if (userChoice === choices[2])
+
+                        $("#uc").html('<img src="images/scissors.png">');
+
+        }
+        else {
+
+                $("#uc").html("Not valid!");
+
+        }
+
+        var computerChoice = [
+
+                '<img src="images/paper.png">'
+
+                '<img src="images/rock.png">'
+
+                '<img src="images/scissors.png">'
+        ];
+
+        if (choices.includes(userChoice)) {
+                var x = Math.ceil(Math.random() * 3);
+
+                if (x <= 1) {
+                        $("#pc").html(computerChoice[0]);
+                }
+                else if (x <= 2) {
+                        $("#pc").html(computerChoice[1]);
+                }
+                else if (x <= 3) {
+                        $("#pc").html(computerChoice[2]);
+                }
+                else
+                        $("#pc").html("Not Valid!");
+}
+
+
+var winner = ["you win", "computer wins", "tie"];
+
+
+
+
+});
+
+
+});
+
+$('#myModal').on('shown.bs.modal', function() {
+$('#myInput').trigger('focus')
+})
 });
 
 //******************TEST EARLY AND OFTEN USING console.log() ******************
@@ -34,4 +98,3 @@ $(document).ready(function(){
 
 
 // DOCUMENT READY FUNCTION
-
